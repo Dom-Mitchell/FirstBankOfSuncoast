4a0743c8c3040999a33e8718eee30c1807af005a
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-08-06 18:31:43 -0400

    Semi-Initial

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Pastel;
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        public int Amount { get; set; }
+        public string Type { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Account { get; set; }
+
+        private List<Transaction> Transactions { get; set; } = new List<Transaction>();
     }
-  }
+
+    class Program
+    {
+
+        static void DisplayGreeting()
+        {
+            Console.WriteLine($"  #####   #####     ######                                       {@"            \       |         /         ".Pastel(Color.Orange)});
+            Console.WriteLine($" #     # #     #    #     #   ##   #    # #    # # #    #  ####  {@"  .          .      |        .        . ".Pastel(Color.Orange)});
+            Console.WriteLine($" #       #          #     #  #  #  ##   # #   #  # ##   # #    # {@"    .         \             /       .   ".Pastel(Color.Orange)});
+            Console.WriteLine($"  #####  #          ######  #    # # #  # ####   # # #  # #      {@"      '  .     \    |      /      .     ".Pastel(Color.Orange)});
+            Console.WriteLine($"       # #          #     # ###### #  # # #  #   # #  # # #  ### {@"            .  ,+~'`^`'~+,/   , '       ".Pastel(Color.Orange)});
+            Console.WriteLine($" #     # #     #    #     # #    # #   ## #   #  # #   ## #    # {@"             .'           '.'           ".Pastel(Color.Orange)});
+            Console.WriteLine($"  #####   #####     ######  #    # #    # #    # # #    #  ####  {@"   --- -- - /               \ - -- ---  ".Pastel(Color.Orange)});
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to C#");
+            DisplayGreeting();
+        }
+    }
 }
