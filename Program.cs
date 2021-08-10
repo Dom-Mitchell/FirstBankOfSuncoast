b1eac876b16772e8b2ff0985117710ea99d62349
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-08-10 16:34:05 -0400

    Refactored for readability

--- Program.cs
+++ Program.cs
@@ -9,14 +9,6 @@ namespace FirstBankOfSuncoast
         static void DisplayGreeting()
         {
             // Sun from https://textart.io/art/tag/sun/1 & Ascii text from https://www.ascii-art-generator.org/
-            // Console.WriteLine($@"  #####   #####     ######                                       {@"            \       |         /         ".Pastel(Color.DarkOrange)}");
-            // Console.WriteLine($@" #     # #     #    #     #   ##   #    # #    # # #    #  ####  {@"  .          .      |        .        . ".Pastel(Color.DarkOrange)}");
-            // Console.WriteLine($@" #       #          #     #  #  #  ##   # #   #  # ##   # #    # {@"    .         \             /       .   ".Pastel(Color.DarkOrange)}");
-            // Console.WriteLine($@"  #####  #          ######  #    # # #  # ####   # # #  # #      {@"      '  .     \    |      /      .     ".Pastel(Color.DarkOrange)}");
-            // Console.WriteLine($@"       # #          #     # ###### #  # # #  #   # #  # # #  ### {@"            .  ,+~'`^`'~+,/   , '       ".Pastel(Color.DarkOrange)}");
-            // Console.WriteLine($@" #     # #     #    #     # #    # #   ## #   #  # #   ## #    # {@"             .'           '.'           ".Pastel(Color.DarkOrange)}");
-            // Console.WriteLine($@"  #####   #####     ######  #    # #    # #    # # #    #  ####  {@"   --- -- - /               \ - -- ---  ".Pastel(Color.DarkOrange)}");
-
             Console.WriteLine($"{"  #####   #####   ".Pastel(Color.LightSkyBlue)}  {"######                                       ".Pastel(Color.SeaGreen)}{@"            \       |         /         ".Pastel(Color.OrangeRed)}");
             Console.WriteLine($"{" #     # #     #  ".Pastel(Color.LightSkyBlue)}  {"#     #   ##   #    # #    # # #    #  ####  ".Pastel(Color.SeaGreen)}{@"  .          .      |        .        . ".Pastel(Color.OrangeRed)}");
             Console.WriteLine($"{" #       #        ".Pastel(Color.LightSkyBlue)}  {"#     #  #  #  ##   # #   #  # ##   # #    # ".Pastel(Color.SeaGreen)}{@"    .         \             /       .   ".Pastel(Color.OrangeRed)}");
@@ -42,135 +34,6 @@ namespace FirstBankOfSuncoast
             Console.WriteLine($"{"############################################################################################################".Pastel(Color.LightSlateGray)}");
         }
 
-        // static string PromptForString(string prompt)
-        // {
-        //     Console.Write(prompt);
-        //     var userInput = Console.ReadLine();
-
-        //     return userInput;
-        // }
-
-        // static int PromptForInteger(string prompt)
-        // {
-        //     Console.Write(prompt);
-        //     int userInput;
-        //     var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
-
-        //     if (isThisGoodInput)
-        //     {
-        //         return userInput;
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
-        //         return 0;
-        //     }
-        // }
-
-        // static double PromptForDouble(string prompt)
-        // {
-        //     Console.Write(prompt);
-        //     double userInput;
-        //     var isThisGoodInput = Double.TryParse(Console.ReadLine(), out userInput);
-
-        //     if (isThisGoodInput)
-        //     {
-        //         return userInput;
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine("Sorry, that isn't a valid input, I'm using 0.0 as your answer.");
-        //         return 0;
-        //     }
-        // }
-
-        // private static void AddUser(UserDatabase database)
-        // {
-        //     var user = new User();
-        //     var correctPinLength = false;
-        //     var correctUserNameLength = false;
-
-
-
-        //     while (!correctPinLength)
-        //     {
-
-        //     }
-
-
-        //     database.AddUser(user);
-        //     database.SaveUsers();
-        // }
-
-        // private static string ExistingUser(UserDatabase database)
-        // {
-        //     var userNameToSearchFor = PromptForString("\nWhat is your username? ");
-        //     //var passwordToSearchFor = PromptForInteger("What is your pin number? ");
-
-        //     database.UserExists(userNameToSearchFor);
-        //     //newMenu();
-        //     return userNameToSearchFor;
-        //     // var passwordToSearchFor = PromptForInteger("What is your pin number? ");
-        //     // database.UserPassWordExists(passwordToSearchFor);
-
-        //     //var user = new User();
-        //     // database.UserNameExists(userNameToSearchFor);
-
-        // }
-
-        // static void newMenu(string usersName)
-        // {
-        //     Console.Clear();
-        //     DisplayGreeting();
-
-        //     var keepGoing = true;
-
-        //     var databaseTransaction = new TransactionDatabase();
-
-        //     while (keepGoing)
-        //     {
-        //         Console.Write("\nWhat do you want to do?\n(W)ithdraw\n(D)eposit\n(B)alance Inquiry\n(Q)uit\n: ");
-        //         var choice = Console.ReadLine().ToUpper();
-
-        //         switch (choice)
-        //         {
-        //             case "W":
-        //                 break;
-        //             case "D":
-        //                 makeDeposit(databaseTransaction, usersName);
-        //                 break;
-        //             case "B":
-        //                 break;
-        //             case "Q":
-        //                 Console.WriteLine();
-        //                 keepGoing = false;
-        //                 break;
-        //             default:
-        //                 Console.WriteLine("\nYour answer was invalid. Please try again!");
-        //                 break;
-        //         }
-        //     }
-
-        // }
-
-        // static void makeDeposit(TransactionDatabase database, string usersName)
-        // {
-        //     var transacion = new Transaction();
-
-        //     transacion.Name = $"{usersName}";
-        //     transacion.Account = PromptForString("Is this your Checking or Savings Account? ");
-        //     transacion.Type = "Deposit";
-        //     transacion.Amount = PromptForDouble("How much money would you like to add? ");
-        //     transacion.TimeStamp = DateTime.Now;
-
-        //     database.AddTransaction(transacion);
-        //     database.SaveTransactions();
-        // }
-
-        // public static void Exit()
-        // {
-        // }
-
         static char PressAnyKey(string prompt)
         {
             Console.WriteLine(prompt);
@@ -178,16 +41,52 @@ namespace FirstBankOfSuncoast
             return keyPressed;
         }
 
-
-        static void Main(string[] args)
+        private static UserDatabase MainMenu()
         {
-            //Console.WriteLine("Welcome to C#");
+            // Database for Users
+            var databaseUser = new UserDatabase();
+            databaseUser.LoadUsers();
 
-            // DisplayExit();
+            var keepGoing = true;
 
-            UserDatabase databaseUser = MainMenu();
-            LoggedInMenu(databaseUser);
+            Console.Clear();
+            DisplayGreeting();
 
+            // While the user hasn't said QUIT yet
+            while (keepGoing && !databaseUser.UserMatches)
+            {
+                Console.Write("\nWhat do you want to do?\n(C)reate Account\n(L)ogin to Account\n(Q)uit\n: ");
+                var choices = Console.ReadLine().ToUpper();
+
+                switch (choices)
+                {
+                    case "C":
+                        databaseUser.CreateUser();
+                        PressAnyKey("\nPress Any Key to Continue! ");
+                        Console.Clear();
+                        DisplayGreeting();
+                        break;
+                    case "L":
+                        databaseUser.ExistingUser();
+                        PressAnyKey("\nPress Any Key to Login! ");
+                        break;
+                    case "Q":
+                        Console.Clear();
+                        DisplayExit();
+                        Environment.Exit(0);
+                        keepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine($"\n{"Your answer was invalid. Please try again!".Pastel(Color.Red)}");
+                        PressAnyKey("\nPress Any Key to Continue! ");
+                        Console.Clear();
+                        DisplayGreeting();
+                        break;
+                }
+
+            }
+
+            return databaseUser;
         }
 
         private static void LoggedInMenu(UserDatabase databaseUser)
@@ -257,105 +156,31 @@ namespace FirstBankOfSuncoast
                             break;
                     }
                 }
-                // PressAnyKey("\nPress Any Key to Continue! ");
             }
         }
 
-        private static UserDatabase MainMenu()
-        {
-            // Database for Users
-            var databaseUser = new UserDatabase();
-            databaseUser.LoadUsers();
-
-            var keepGoing = true;
-
-            Console.Clear();
-            DisplayGreeting();
-
-            // While the user hasn't said QUIT yet
-            while (keepGoing && !databaseUser.UserMatches)
-            {
-                Console.Write("\nWhat do you want to do?\n(C)reate Account\n(L)ogin to Account\n(Q)uit\n: ");
-                var choices = Console.ReadLine().ToUpper();
-
-                switch (choices)
-                {
-                    case "C":
-                        databaseUser.CreateUser();
-                        PressAnyKey("\nPress Any Key to Continue! ");
-                        Console.Clear();
-                        DisplayGreeting();
-                        break;
-                    case "L":
-                        databaseUser.ExistingUser();
-                        PressAnyKey("\nPress Any Key to Login! ");
-                        break;
-                    case "Q":
-                        Console.Clear();
-                        DisplayExit();
-                        Environment.Exit(0);
-                        keepGoing = false;
-                        break;
-                    default:
-                        Console.WriteLine($"\n{"Your answer was invalid. Please try again!".Pastel(Color.Red)}");
-                        PressAnyKey("\nPress Any Key to Continue! ");
-                        Console.Clear();
-                        DisplayGreeting();
-                        break;
-                }
-
-            }
-
-            return databaseUser;
-        }
-
-        private static void BalaneInquiry(UserDatabase user, TransactionDatabase transactions)
+        private static void Withdraw(UserDatabase user, TransactionDatabase transactions)
         {
             var correctChoice = false;
             while (!correctChoice)
             {
-                Console.WriteLine($"\nWould you like to view your Checking or Savings Account, {user.SingularUser.UserName}? (Checking/Savings) ");
+                Console.WriteLine($"\nWould you like to withdraw to your Checking or Savings Account, {user.SingularUser.UserName}? (Checking/Savings) ");
                 var userSelection = Console.ReadLine().ToLower();
 
                 if (userSelection == "c" || userSelection == "checking")
                 {
-                    Console.WriteLine($"\nChecking Balance: ${transactions.checkingTotal(user.SingularUser)}");
+                    transactions.AddWithdrawFromChecking(user.SingularUser);
                     break;
                 }
                 else if (userSelection == "s" || userSelection == "savings")
                 {
-                    Console.WriteLine($"\nSavings Balance: ${transactions.savingsTotal(user.SingularUser)}");
+                    transactions.AddWithdrawFromSavings(user.SingularUser);
                     break;
                 }
                 else
                 {
                     Console.WriteLine($"\n{"Your answer was invalid. Please try again!".Pastel(Color.Red)}");
                 }
-
-            }
-        }
-
-        private static void Transfer(UserDatabase user, TransactionDatabase transactions)
-        {
-            var correctChoice = false;
-            while (!correctChoice)
-            {
-                Console.WriteLine($"\nWould you like to transfer from your Checking or Savings Account, {user.SingularUser.UserName}? (Checking/Savings) ");
-                var userSelection = Console.ReadLine().ToLower();
-
-                if (userSelection == "c" || userSelection == "checking")
-                {
-                    transactions.TransferToSaving(user.SingularUser);
-                }
-                else if (userSelection == "s" || userSelection == "savings")
-                {
-                    transactions.TransferToChecking(user.SingularUser);
-                }
-                else
-                {
-                    Console.WriteLine($"\n{"Your answer was invalid. Please try again!".Pastel(Color.Red)}");
-                }
-                break;
 
             }
         }
@@ -385,22 +210,47 @@ namespace FirstBankOfSuncoast
             }
         }
 
-        private static void Withdraw(UserDatabase user, TransactionDatabase transactions)
+        private static void Transfer(UserDatabase user, TransactionDatabase transactions)
         {
             var correctChoice = false;
             while (!correctChoice)
             {
-                Console.WriteLine($"\nWould you like to withdraw to your Checking or Savings Account, {user.SingularUser.UserName}? (Checking/Savings) ");
+                Console.WriteLine($"\nWould you like to transfer from your Checking or Savings Account, {user.SingularUser.UserName}? (Checking/Savings) ");
                 var userSelection = Console.ReadLine().ToLower();
 
                 if (userSelection == "c" || userSelection == "checking")
                 {
-                    transactions.AddWithdrawFromChecking(user.SingularUser);
+                    transactions.TransferToSaving(user.SingularUser);
                     break;
                 }
                 else if (userSelection == "s" || userSelection == "savings")
                 {
-                    transactions.AddWithdrawFromSavings(user.SingularUser);
+                    transactions.TransferToChecking(user.SingularUser);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"\n{"Your answer was invalid. Please try again!".Pastel(Color.Red)}");
+                }
+            }
+        }
+
+        private static void BalaneInquiry(UserDatabase user, TransactionDatabase transactions)
+        {
+            var correctChoice = false;
+            while (!correctChoice)
+            {
+                Console.WriteLine($"\nWould you like to view your Checking or Savings Account, {user.SingularUser.UserName}? (Checking/Savings) ");
+                var userSelection = Console.ReadLine().ToLower();
+
+                if (userSelection == "c" || userSelection == "checking")
+                {
+                    Console.WriteLine($"\nChecking Balance: ${transactions.checkingTotal(user.SingularUser)}");
+                    break;
+                }
+                else if (userSelection == "s" || userSelection == "savings")
+                {
+                    Console.WriteLine($"\nSavings Balance: ${transactions.savingsTotal(user.SingularUser)}");
                     break;
                 }
                 else
@@ -410,5 +260,13 @@ namespace FirstBankOfSuncoast
 
             }
         }
+
+        static void Main(string[] args)
+        {
+            UserDatabase databaseUser = MainMenu();
+            LoggedInMenu(databaseUser);
+
+        }
+
     }
 }
